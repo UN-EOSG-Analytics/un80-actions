055f86cf3576627e21dfbe3ff02e8ef517626c46
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-11-11 23:53:21 -0500

    style explainer

--- src/components/ExplainerText.tsx
+++ src/components/ExplainerText.tsx
@@ -1,13 +1,13 @@
 export function ExplainerText() {
     return (
-        <div className="text-muted-foreground mt-4 sm:text-justify w-full lg:max-w-[818px] text-left">
-            <p className="leading-tight mb-0">
+        <div className="text-gray-600 mt-2 mb-8 sm:text-justify w-full lg:max-w-[830px] text-left">
+            <p className="leading-relaxed mb-0">
                 This Dashboard is an annex to the{" "}
                 <a
                     href="https://www.un.org/un80-initiative/en"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-un-blue hover:text-shuttle-gray transition-colors"
+                    className="font-bold text-un-blue hover:underline transition-all"
                 >
                     UN80 Initiative Action Plan
                 </a>
@@ -16,18 +16,18 @@ export function ExplainerText() {
                     href="https://www.un.org/un80-initiative/en"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-un-blue hover:text-shuttle-gray transition-colors"
+                    className="font-bold text-un-blue hover:underline transition-all"
                 >
                     UN80 Initiative
                 </a>{" "}
                 workstreams in a single reference. This Dashboard also lists designated leads
                 for each work package, as well as their individual action items (derived from
-                paragraphs in the SG&apos;s reports on{" "}
+                paragraphs in the SG&apos;s reports on the{" "}
                 <a
                     href="https://www.un.org/un80-initiative/en"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-un-blue hover:text-shuttle-gray transition-colors"
+                    className="font-bold text-un-blue hover:underline transition-all"
                 >
                     UN80 Initiative
                 </a>
