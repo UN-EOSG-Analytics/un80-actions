49cdfe13be0accbf3c8dc27a7f754786a1b0084a
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2026-01-28 16:22:03 -0500

    user table

--- python/users/01-fetch_users.py
+++ python/users/01-fetch_users.py
@@ -1,7 +1,5 @@
-import os
 from api.airtable import fetch_airtable_table
 
-fetch_airtable_table(table_id=, base_id=)
+USER_TABLE_ID = "tbl38pZDARtrQKbHT"
 
-BASE_ID = 
-TABLE_ID = os.environ["AIRTABLE_TABLE_ID"]
+df = fetch_airtable_table(table_id=USER_TABLE_ID)
